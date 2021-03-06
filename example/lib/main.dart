aa332888e70050add8545be8fe58361c675f2107
Gordon Hayes <gordon@beamm.co>
2021-03-06 11:28:18 -0600

    docs: add custom particle path

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -56,6 +56,31 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 
+  /// A custom Path to paint stars.
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -76,6 +101,7 @@ class _MyAppState extends State<MyApp> {
               Colors.orange,
               Colors.purple
             ], // manually specify the colors to be used
+            createParticlePath: drawStar, // define a custom shape/path.
           ),
         ),
         Align(
@@ -84,7 +110,7 @@ class _MyAppState extends State<MyApp> {
               onPressed: () {
                 _controllerCenter.play();
               },
-              child: _display('blast')),
+              child: _display('blast\nstars')),
         ),
 
         //CENTER RIGHT -- Emit left
